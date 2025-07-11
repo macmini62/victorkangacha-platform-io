9d4933b1725934e1dd39506aed5264498434ac93
macmini62 <victorkangacha@gmail.com>
2025-07-11 17:00:38 +0300

    Added cv download buttons

--- src/components/sections/about-detail.tsx
+++ src/components/sections/about-detail.tsx
@@ -19,7 +19,7 @@ export default function AboutDetail() {
     fetch("/cv_document/CV.pdf").then((response) => {
       response.blob().then((blob) => {
         const fileURL = window.URL.createObjectURL(blob);
-        let alink = document.createElement("a");
+        const alink = document.createElement("a");
         alink.href = fileURL;
         alink.download = "CV.pdf";
         alink.click();
